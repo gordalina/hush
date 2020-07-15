82f541e310c1a49f54d43b7209aafb4c130ab586
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-07-15 14:55:35 -0700

    Lower minimum elixir version to 1.9

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Hush.MixProject do
     [
       app: :hush,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       deps: deps(),
       docs: docs(),
       description: description(),
