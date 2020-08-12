94a92a77063d2125e002a29ce7272ae60940c72e
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-08-11 21:12:35 -0700

    Always show test trace in ex_check

--- .check.exs
+++ .check.exs
@@ -7,11 +7,11 @@
     ## curated tools may be disabled (e.g. the check for compilation warnings)
     # {:compiler, false},
     ## ...or have command & args adjusted (e.g. enable skip comments for sobelow)
-    {:sobelow, "mix sobelow --exit --skip"}
+    {:sobelow, "mix sobelow --exit --skip"},
     ## ...or reordered (e.g. to see output from dialyzer before others)
     # {:dialyzer, order: -1},
     ## ...or reconfigured (e.g. disable parallel execution of ex_unit in umbrella)
-    # {:ex_unit, umbrella: [parallel: false]},
+    {:ex_unit, "mix test --trace"}
     ## custom new tools may be added (Mix tasks or arbitrary commands)
     # {:my_task, "mix my_task", env: %{"MIX_ENV" => "prod"}},
     # {:my_tool, ["my_tool", "arg with spaces"]}
