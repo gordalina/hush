487cf84d844a057c7151b79a5ef05a6a43e54592
Samuel Gordalina <samuel.gordalina@gmail.com>
2022-08-23 14:38:06 -1000

    Update release script

--- bin/release.exs
+++ bin/release.exs
@@ -7,20 +7,42 @@ defmodule Hush.Release do
 
     ensure_git_clean()
 
+    replace_infile("CHANGELOG.md", ~r/## Next/, "## v#{version}")
     replace_infile("mix.exs", ~r/@version \".*\"/, "@version \"#{version}\"")
-    replace_infile("README.md", ~r/{:hush, \"~> .*\"}/, "{:hush, \"~> #{version}\"}")
+
+    replace_infile(
+      "README.md",
+      ~r/{:hush, \"~> .*\"}/,
+      "{:hush, \"~> #{version}\"}"
+    )
 
     show_git_diff()
     ensure_user_wants_release()
 
-    git(["add", "README.md", "mix.exs"])
-    git(["commit", "-m", "Bump version to v#{version}"])
-    git(["tag", "v#{version}"])
+    git(["add", "CHANGELOG.md", "README.md", "mix.exs"])
+    git(["commit", "-m", "v#{version}"])
+    git(["tag", "v#{version}", "-m", "v#{version}"])
     git(["push"])
     git(["push", "--tags"])
   end
 
-  def run([]), do: IO.puts("Need to supply an argument with a version in the format: 0.0.0")
+  def run([]) do
+    tags =
+      git(["tag", "--list", "--sort=-v:refname"])
+      |> then(fn {contents, 0} -> contents end)
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.slice(0..5)
+
+    """
+    Error: Missing version
+    Usage: bin/release.exs 0.0.0
+
+    Last 5 tags:
+    - #{tags |> Enum.join("\n- ")}
+    """
+    |> IO.write()
+  end
 
   defp replace_infile(file, pattern, subst) do
     file
@@ -39,7 +61,7 @@ defmodule Hush.Release do
 
   defp ensure_user_wants_release() do
     if IO.gets("Do you want to release this version [y/N]? ") != "y\n" do
-      git(["checkout", "HEAD", "README.md", "mix.exs"])
+      git(["checkout", "HEAD", "CHANGELOG.md", "README.md", "mix.exs"])
       raise RuntimeError, message: "Release aborted"
     end
   end
